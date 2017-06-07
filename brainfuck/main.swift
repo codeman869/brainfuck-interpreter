94fb7d7bf4458886fb78f0e0521d70b18ffe8353
Cody Deckard <cjdeckard@gmail.com>
2017-06-06 19:28:40 -0700

    Initial Commit
    Creates the main.swift file and adds basic branching functionality
    for the application. Not all commands implemented, only >, <, .,
    +, and -.

--- brainfuck/main.swift
+++ brainfuck/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-print("Hello, World!")
+public class BFInterpreter {
+    
+    var memory = Array(repeating: 0, count: 30000)
+    var codePointer = 0
+    var memPointer = 0
+    
+    public func run(_ code: String) -> String {
+        var output : [Int] = []
+        let codeArray = Array(code.characters)
 
+        while self.codePointer >= 0 && self.codePointer < codeArray.count {
+            switch codeArray[codePointer] {
+            case ">":
+                self.memPointer += 1
+            case "<":
+                self.memPointer -= 1
+            case "+":
+                self.memory[memPointer] = (self.memory[memPointer] + 1) % 256
+            case "-":
+                self.memory[memPointer] = (self.memory[memPointer] - 1) % 256
+            case ".":
+                output.append(memory[memPointer])
+            case "[":
+                break
+            case "]":
+                break
+            default:
+                break
+            }
+        
+            if memPointer < 0 || memPointer >= memory.count {
+                break
+            }
+        }
+    
+        return output.reduce("") { return $0 + "\(UnicodeScalar($1)!)"}
+    }
+}
